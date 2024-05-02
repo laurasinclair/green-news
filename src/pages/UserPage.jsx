72e7e9c78c590e1a09d8cdef150e6686f83029e6
Laura Sinclair <l.sinclair@tuta.io>
2024-05-02 15:18:43 +0200

    User page - Displays saved articles

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -2,7 +2,7 @@ import { Container, Row, Col } from 'react-bootstrap'
 import { useState, useEffect } from 'react'
 import { Link, useParams, useNavigate } from 'react-router-dom'
 
-import { Button, Hero, Feed } from '@components'
+import { Button, Hero, Feed, SaveBtn } from '@components'
 
 export default function UserPage() {
 	const navigate = useNavigate()
@@ -29,12 +29,31 @@ export default function UserPage() {
 		}
 	}, [users, currentUser])
 
-
+	// updating page title to user name
 	useEffect(() => {
 		if (currentUser && currentUser.firstName) {
 			document.title = `${window.name} | ${currentUser.firstName} ${currentUser.lastName} ⛭ User settings`
 		}
 	}, [currentUser])
+
+	// displaying saved articles
+	const [allArticles, setAllArticles] = useState([])
+
+	useEffect(() => {
+		fetch(`https://newsapi.org/v2/everything?q=trees&apiKey=${import.meta.env.VITE_NEWS_API_TOKEN}`)
+			.then((resp) => resp.json())
+			.then((data) => setAllArticles(data.articles))
+			.catch((err) => setError(`Data couldn't be fetched - ${err}`))
+	}, [])
+
+	// matching the url with the right article
+	function getSlug(str) {
+		const tempString = str
+			.replaceAll(/[^a-zA-Z0-9]/g, '-')
+			.toLowerCase()
+			.substring(0, 50)
+		return tempString.replace(/-+/g, '-').replace(/-$/, '')
+	}
 
 	return (
 		<>
@@ -65,12 +84,22 @@ export default function UserPage() {
 										Nothing to show today :(
 									</Col>
 								) : (
-									currentUser.savedArticles.map((article) => {
-										return (
-											<Col md="6" lg="4" className="mb-4">
-												{article}
-											</Col>
-										)
+									allArticles.map((article, i) => {
+										for (let articleSlug of currentUser.savedArticles) {
+											if (articleSlug === getSlug(article.title)) {
+												return (
+													<Col md="6" lg="4" key={i} className="mb-4">
+													<Link to={`articles/${getSlug(article.title)}`}>
+														<div>
+															<h3>{article.title}</h3>
+															<div>{article.description}</div>
+															<SaveBtn articleSlug={getSlug(article.title)} />
+														</div>
+													</Link>
+												</Col>
+												)
+											}
+										}
 									})
 								)}
 							</>
