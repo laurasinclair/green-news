e23852134262de41827fa7f71a5fa7fc0cd89310
Laura Sinclair <l.sinclair@tuta.io>
2024-05-02 14:49:24 +0200

    User space link in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Container, Row, Col } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
+import { PersonCircle } from 'react-bootstrap-icons'
 import { Logo } from '@components';
 import styles from './styles/Navbar.module.sass'
 
@@ -11,6 +12,11 @@ export default function Navbar() {
 					<Col>
 						<NavLink to="/">
 							<Logo size="xs" hasText />
+						</NavLink>
+					</Col>
+					<Col className="d-flex align-items-center justify-content-end">
+						<NavLink to="/user/johndoe">
+							<PersonCircle size="30" />
 						</NavLink>
 					</Col>
 				</Row>
