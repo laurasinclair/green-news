29ea14d831db41a0cb91cea34aae967d4b2e34cd
Laura Sinclair <l.sinclair@tuta.io>
2024-05-03 17:51:28 +0200

    UseContext fix

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,13 +2,15 @@ import { Row, Col } from 'react-bootstrap'
 import ReactPaginate from 'react-paginate'
 import { useEffect, useState, useContext } from 'react'
 import { Link } from 'react-router-dom'
-import { ArrowLeftCircleFill, ArrowRightCircleFill } from 'react-bootstrap-icons'
-import { UserContext, SaveBtn } from '@components'
+import placeholder from '@img/placeholder_1-1.jpg'
+import { ArrowLeftCircleFill, ArrowRightCircleFill, ArrowRight} from 'react-bootstrap-icons'
+import { Button, SaveBtn } from '@components'
 
 import styles from './styles/Feed.module.sass'
+import { useUserContext } from '../components/UserContext'
 
 export default function Feed(props) {
-	const { currentUser } = useContext(UserContext);
+	const { currentUser } = useUserContext();
 
 	// fetching the data
 	const [data, setData] = useState([])
@@ -51,7 +53,6 @@ export default function Feed(props) {
 	return (
 		<div className={styles.feed}>
 			<h2>News feed</h2>
-			
 
 			<Row>
 				{loading ? (
@@ -69,6 +70,17 @@ export default function Feed(props) {
 					filteredData && (
 						<>
 							<Col>
+								<Row>
+									<Col md="6">
+										<Link to={`articles/${getSlug(filteredData[0].title)}`}>
+											<div className={styles.featuredImage}>
+												<h3>{filteredData[0].title}</h3>
+												<img src={filteredData[0].urlToImage} alt={filteredData[0].title} />
+											</div>
+										</Link>
+									</Col>
+								</Row>
+
 								<div className="mb-4">{data.articles.length} articles</div>
 								<Row>
 									{filteredData &&
@@ -76,20 +88,31 @@ export default function Feed(props) {
 											return (
 												<Col md="6" lg="4" key={i} className="mb-4">
 													<Link to={`articles/${getSlug(article.title)}`}>
-														<div className={styles.feed_article}>
-															<h3>{article.title}</h3>
-															<div>{article.description}</div>
-															
-															{currentUser.userId && (
-																<SaveBtn articleSlug={getSlug(article.title)} />
-															)}
+														<div className={styles.feed_articleCard}>
+															<div className={styles.feed_articleCard_thumbnail}>
+																<img src={article.urlToImage || placeholder} alt={article.title | window.name} />
+															</div>
+															<div className={styles.feed_articleCard_body}>
+																<h3>{article.title}</h3>
+																<p>{article.description}</p>
+																<Row className="flex-column flex-sm-row">
+																	{currentUser?.userId && (
+																		<Col>
+																			<SaveBtn articleSlug={getSlug(article.title)} />
+																		</Col>
+																	)}
+																	<Col>
+																		<Button link={`articles/${getSlug(article.title)}`} text="Read more" fullWidth iconRight={<ArrowRight size="18" />} />
+																	</Col>
+																</Row>
+															</div>
 														</div>
 													</Link>
 												</Col>
 											)
 										})}
 
-									<ReactPaginate containerClassName={styles.feed_pagination} pageClassName={styles.pageItem} activeClassName={styles.active} onPageChange={(event) => setPage(event.selected)} pageCount={Math.ceil(data.articles.length / perPage)} breakLabel="..." previousLabel={<ArrowLeftCircleFill color="#5c736c" size="40" className="mt-2 me-3" />} nextLabel={<ArrowRightCircleFill color="#5c736c" size="40" className="mt-2 ms-3" />} />
+									<ReactPaginate containerClassName={styles.feed_pagination} pageClassName={styles.pageItem} activeClassName={styles.active} onPageChange={(event) => setPage(event.selected)} pageCount={Math.ceil(data.articles.length / perPage)} breakLabel="..." previousLabel={<ArrowLeftCircleFill color="#aab5a2" size="40" className="mt-2 me-3" />} nextLabel={<ArrowRightCircleFill color="#aab5a2" size="40" className="mt-2 ms-3" />} />
 								</Row>
 							</Col>
 						</>
