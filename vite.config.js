ce98c614dec90fb15186cc825df67e1de078d45c
Laura Sinclair <l.sinclair@tuta.io>
2024-05-07 14:05:42 +0200

    Vite config: minify css

--- vite.config.js
+++ vite.config.js
@@ -7,14 +7,15 @@ export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: {
-
 			'@': path.resolve(__dirname, 'src'),
 			'@components': `${path.resolve(__dirname, 'src/components/')}`,
 			'@assets': `${path.resolve(__dirname, 'src/assets/')}`,
 			'@img': `${path.resolve(__dirname, 'src/assets/images/')}`,
-			'@pages': path.resolve(__dirname, 'src/pages'),
-			// public: `${path.resolve(__dirname, "./public/")}`,
-			// types: `${path.resolve(__dirname, "./src/@types")}`,
+			'@pages': path.resolve(__dirname, 'src/pages')
 		},
 	},
+	build: {
+		cssCodeSplit: false,
+		cssMinify: 'esbuild'
+	}
 })
