55a8b2883fcc634eb5a81cd8a86bcc4bba8dfb71
Laura Sinclair <l.sinclair@tuta.io>
2024-05-07 14:06:15 +0200

    Vite config: remove console.log() on production

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path';
+import removeConsole from "vite-plugin-remove-console";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), removeConsole()],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, 'src'),
