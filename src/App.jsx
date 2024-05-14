2145de865c436afaf0a4e8a49ce48e881ba23c2c
Laura Sinclair <l.sinclair@tuta.io>
2024-05-14 17:34:25 +0200

    User page is up-to-date with new API

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
 import './App.sass'
 import { HomePage, Article, UserPage, NotFound } from '@pages'
-import { UserContextProvider, FeedContextProvider, Navbar, Footer, Main } from '@components'
+import { Navbar, Footer, Main } from '@components'
+import FeedContextProvider from '@components/FeedContext'
+import UserContextProvider from '@components/UserContext'
+
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
 	return (
 		<div className="App">
 			<UserContextProvider>
+			<FeedContextProvider>
 				<Navbar />
 				
 				<Main>
 					<Routes>
-						<FeedContextProvider>
 							<Route path="/" element={<HomePage />} />
 							<Route path="/articles/:articleSlug" element={<Article />} />
 							<Route path="/user/johndoe" element={<UserPage />} />
-						</FeedContextProvider>
 
 						<Route path="*" element={<NotFound />} />
 					</Routes>
 				</Main>
 
 			<Footer />
+			</FeedContextProvider>
 			</UserContextProvider>
 		</div>
 	)
