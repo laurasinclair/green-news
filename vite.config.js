f569100dbeafcc4d071cc820cc6e212d0af41c9b
Laura Sinclair <l.sinclair@tuta.io>
2024-05-16 10:54:41 +0200

    chore: Update base directory in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/projects/green-news/', // Set the base directory
+	base: './', // Set the base directory
 	plugins: [react()],
 	resolve: {
 		alias: {
