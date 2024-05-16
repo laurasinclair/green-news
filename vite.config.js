093ca99e73221e1f6ce1791a30ad5d215b9dd513
Laura Sinclair <l.sinclair@tuta.io>
2024-05-16 10:38:31 +0200

    Could be it

--- vite.config.js
+++ vite.config.js
@@ -8,14 +8,15 @@ export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: {
-			'@': path.resolve(__dirname, 'src'),
-			'@components': `${path.resolve(__dirname, 'src/components/')}`,
-			'@assets': `${path.resolve(__dirname, 'src/assets/')}`,
-			'@img': `${path.resolve(__dirname, 'src/assets/images/')}`,
-			'@pages': path.resolve(__dirname, 'src/pages')
+			'@': path.resolve(__dirname, './src'),
+			'@components': `${path.resolve(__dirname, './src/components/')}`,
+			'@assets': `${path.resolve(__dirname, './src/assets/')}`,
+			'@img': `${path.resolve(__dirname, './src/assets/images/')}`,
+			'@pages': path.resolve(__dirname, './src/pages')
 		},
 	},
 	build: {
+		outDir: '/projects/green-news/',
 		cssCodeSplit: false,
 		cssMinify: 'esbuild'
 	}
