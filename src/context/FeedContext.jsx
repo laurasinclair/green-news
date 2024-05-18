3d0e91e075a9a68f729f7d9d47e06d111c1dff1e
Laura Sinclair <l.sinclair@tuta.io>
2024-05-18 02:49:32 +0200

    Uses local storage to store articles

--- src/context/FeedContext.jsx
+++ src/context/FeedContext.jsx
@@ -8,20 +8,26 @@ export default function FeedContextProvider({ children }) {
 	// fetching the data
 	const [data, setData] = useState([])
 	const [error, setError] = useState('')
-	const [isDataFetched, setIsDataFetched] = useState(false)
 
 	const fetchData = () => {
 		axios
 			.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=nature&api-key=${import.meta.env.VITE_NYTIMES_API_TOKEN}`)
-			.then(({data}) => setData(data.response.docs))
+			.then(({ data }) => {
+				console.log(data.response.docs)
+				setData(data.response.docs)
+				localStorage.setItem('articles', JSON.stringify(data.response.docs));
+			})
 			.catch((error) => setError(error))
-		}
+	}
 
-		useEffect(() => {
-			if (!isDataFetched) {
-				fetchData();
-			}
-		}, [isDataFetched])
+	useEffect(() => {
+		const storedData = localStorage.getItem('articles')
+		if (storedData) {
+			setData(JSON.parse(storedData))
+		} else {
+			fetchData();
+		}
+	}, [])
 
 	return <FeedContext.Provider value={{ data, setData, error, setError }}>{children}</FeedContext.Provider>
 }
