2713cebeefc7a092460e83d67c65b5d95ca7b237
Laura Sinclair <l.sinclair@tuta.io>
2024-06-11 20:20:23 +0200

    Fetch news from backend instead of frontend

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path';
-import removeConsole from "vite-plugin-remove-console";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: process.env.NODE_ENV === 'production' ? '/projects/green-news/' : '/',
-	plugins: [react(), removeConsole()],
+	base: '/',
+	plugins: [react()],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, 'src'),
