e4d7b58c4aec9bead36db9f6aed71aa1f0f425bd
Laura Sinclair <l.sinclair@tuta.io>
2024-06-14 14:48:54 +0200

    Reorganising css files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.scss'
+import './styles/index.sass';
 import { BrowserRouter as Router } from 'react-router-dom'
 
 window.name = "Green News";
